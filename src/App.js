1b5e7080543badd18c6e683992df8daf5e999c0c
tanmay.patil <tp702832@gmail.com>
2023-01-08 20:30:33 +0530

    added invalid route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
 import Home from './components/Home/Home';
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
 import Login from './components/Login/Login';
 import Jobs from './components/Jobs/Jobs';
+import Invalid from './components/Invalid/Invalid';
 function App() {
   return (
   <BrowserRouter>
@@ -13,6 +14,7 @@ function App() {
    <Route  path="/"  exact element={<Home/>}/>
    <Route  path="/login" exact  element={<Login/>}/>
    <Route  path="/jobs"   element={<Jobs/>}/>
+   <Route  path="*"   element={<Invalid/>}/>
    </Routes>
  </BrowserRouter>
   );
---
+++ src/components/Invalid/Invalid.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Invalid = () => {
+  return (
+    <div>Opps!! This is an invalid Url.</div>
+  )
+}
+export default Invalid
